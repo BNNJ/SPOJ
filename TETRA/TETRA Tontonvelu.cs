bd603140d5a436ecc9d402da0c7f85c0b1c9eaae
TontonVelu <53855372+TontonVelu@users.noreply.github.com>
2019-08-07 20:11:08 +0200

    Create TETRA Tontonvelu.cs

---
+++ TETRA/TETRA Tontonvelu.cs
@@ -0,0 +1,63 @@
+using System;
+
+public class Program
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] Edges;
+            double dVolumeTet;
+            double dVolumeShp;
+            double S1, S2, S3, S4;
+
+
+            for(int i= 0; i != N; i++)
+            {     
+                Edges = Console.ReadLine().Split(' ');
+                dVolumeTet = Volumetet(double.Parse(Edges[0]), double.Parse(Edges[1]), double.Parse(Edges[2]), double.Parse(Edges[3]), double.Parse(Edges[4]), double.Parse(Edges[5]));
+
+                S1 = Area(double.Parse(Edges[0]), double.Parse(Edges[1]), double.Parse(Edges[3]));
+                S2 = Area(double.Parse(Edges[0]), double.Parse(Edges[2]), double.Parse(Edges[4]));
+                S3 = Area(double.Parse(Edges[1]), double.Parse(Edges[5]), double.Parse(Edges[2]));
+                S4 = Area(double.Parse(Edges[3]), double.Parse(Edges[4]), double.Parse(Edges[5]));
+
+                dVolumeShp = Math.Round(VolumShp(S1, S2, S3, S4, dVolumeTet),4);
+                Console.WriteLine(dVolumeShp);
+            }
+        }
+
+        public static double VolumShp(double s1, double s2, double s3, double s4, double vol)
+        {
+            // radius of sphere = (3*vol)/SurfaceArea 
+            double radius;
+            double VolShp;
+            radius = (3 * vol) / (s1 + s2 + s3 + s4);
+            VolShp = 4 / 3 * Math.PI * Math.Pow(radius, 3);
+            return VolShp;
+        }
+        public static double Volumetet(double a, double b, double c, double d, double e, double f)
+        {
+            double volume;
+
+            a = Math.Pow(a, 2);
+            b = Math.Pow(b, 2);
+            c = Math.Pow(c, 2);
+            d = Math.Pow(d, 2);
+            e = Math.Pow(e, 2);
+            f = Math.Pow(f, 2);
+
+            volume = Math.Sqrt(4*a*b*c-a*(b+c-f)-b*(a+c-e)-c*(a+b-d)+(b+c-f)-(c+a-e)*(a+b-d))/12;
+            return volume;
+        }
+
+        public static double Area(double a, double b, double c)
+        {
+            double Area;
+            double P;
+
+            P = (a + b + c) / 2;
+            Area = Math.Sqrt(P * (P - a) * (P - b) * (P - c));
+
+            return Area;
+        }
+    }
