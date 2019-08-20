0623fa7a23edde062cc4db07205b72f59cdf920c
BNNJ <BNNJ1974@gmail.com>
2019-08-20 22:53:45 +0200

    SHPATH: added debug mode - comment/uncomment the debug define at the top of source file

--- SHPATH/complete.c
+++ SHPATH/complete.c
@@ -3,6 +3,8 @@
 #include <limits.h>
 #include <string.h>
 
+// #define DEBUG
+
 typedef struct	s_node
 {
 	char			name[16];
@@ -21,11 +23,23 @@ typedef struct	s_graph
 	int				nb_nodes;
 }				t_graph;
 
-#define HEAP
+void		print_matrix(int **m, int size)
+{
+	printf("----- MATRIX -----\nsize: %d\n", size);
+	printf("    ");
+	for (int i = 0; i < size; ++i)
+		printf("%4d", i);
+	printf("\n");
+	for (int i = 0; i < size; ++i)
+	{
+		printf("%4d", i);
+		for (int j = 0; j < size; ++j)
+			printf("%4d", m[i][j]);
+		printf("\n");
+	}
+}
 
 /*************************** HEAP HANDLING ************************************/
-
-#ifdef HEAP
 
 static t_node		*heap[10000];
 static int			heap_size = 0;
@@ -90,51 +104,6 @@ t_node		*pop(void)
 	return (n);
 }
 
-/******************************** QUEUE ***************************************/
-#elif
-
-t_node	*queue = NULL;
-
-static inline void		push(t_node *node)
-{
-	t_node	*cur = queue;
-	
-	if (cur == NULL)
-		queue = node;
-	else
-	{
-		while (cur->next && cur->next->cost < node->cost)
-		{
-			printf("%d\n", cur->index);
-			cur = cur->next;
-		}
-		if (cur == NULL)
-		{
-			node->next = queue;
-			queue = node;
-		}
-		else
-		{
-			node->next = cur->next;
-			cur->next = node;
-		}
-	}
-}
-
-static inline t_node	*pop()
-{
-	t_node	*node = queue;
-	queue = queue->next;
-	node->next = NULL;
-	return (node);
-}
-
-static inline int		queue_is_empty()
-{
-	return (queue == NULL);
-}
-
-#endif
 /******************************* SOLVER ***************************************/
 
 void		update_neighbors(t_graph *g, t_node *c)
@@ -144,10 +113,11 @@ void		update_neighbors(t_graph *g, t_node *c)
 	for (int i = 0; i < c->nb_links; ++i)
 	{
 		n = &g->nodes[c->links[i]];
-		if (n->cost > c->cost + g->edges[n->index][c->index])
-			n->cost = c->cost + g->edges[n->index][c->index];
-		if (n->visited == 0)
+		if (n->cost > c->cost + g->edges[c->index][n->index])
+		{
+			n->cost = c->cost + g->edges[c->index][n->index];
 			push(n);
+		}
 	}
 }
 
@@ -155,11 +125,14 @@ void		find_path(t_graph *g, int s, int e)
 {
 	t_node	*c = NULL;
 
+#ifdef DEBUG
 	printf("------------------------------\n");
 	printf("start:	%d %s\nend:	%d %s\n", s, g->nodes[s].name, e, g->nodes[e].name);
+#endif
+
 	g->nodes[s].cost = 0;
 	push(&g->nodes[s]);
-	while (!queue_is_empty() && c != &g->nodes[e])
+	while (heap_size > 0 && c != &g->nodes[e])
 	{
 		c = pop();
 		if (c->visited == 0)
@@ -168,7 +141,7 @@ void		find_path(t_graph *g, int s, int e)
 			update_neighbors(g, c);
 		}
 	}
-	if (c->index == e)
+	if (c == &g->nodes[e])
 		printf("%d\n", c->cost);
 	else
 		printf("no path found\n");
@@ -188,6 +161,7 @@ void		solve(t_graph *g)
 	char	start[16], end[16];
 	int		s, e;
 
+
 	scanf("%s %s", start, end);
 	for (int i = 0; i < g->nb_nodes; ++i)
 	{
@@ -199,6 +173,9 @@ void		solve(t_graph *g)
 		else if (strcmp(end, g->nodes[i].name) == 0)
 			e = i;
 	}
+#ifdef DEBUG
+	printf("start: %d end: %d\n", s, e);
+#endif
 	find_path(g, s, e);
 }
 
@@ -211,6 +188,9 @@ int			create_node(t_node *n, int i, int **edges)
 	n->index = i;
 	scanf("%s", n->name);
 	scanf("%d", &n->nb_links);
+#ifdef DEBUG
+	printf("----------\n%d %s\n", i, n->name);
+#endif
 	if (!(n->links = malloc(sizeof(int) * n->nb_links)))
 		return (0);
 	for (int j = 0; j < n->nb_links; ++j)
@@ -218,12 +198,12 @@ int			create_node(t_node *n, int i, int **edges)
 		scanf("%d %d", &index, &cost);
 		--index;
 		n->links[j] = index;
-//		printf(">> %d / %d <<\n", j, n->nb_links);
-//		printf(">>>> %d / %d : %d <<\n", i, index, cost);
+#ifdef DEBUG
+		printf(">> %d / %d <<\n", j, n->nb_links);
+		printf(">>>> %d / %d : %d <<\n", i, index, cost);
+#endif
 		edges[i][index] = cost;
-		edges[index][i] = cost;
 	}
-//	printf("HELLO\n");
 	return (1);
 }
 
@@ -238,33 +218,35 @@ int			main(void)
 {
 	int		nb_tests = 0, nb_paths = 0;
 	t_graph	graph;
-	char	tmp[8];
 
 	scanf("%d",&nb_tests);
 	while (--nb_tests >= 0)
 	{
 		graph = (t_graph){0};
 		scanf("%d", &graph.nb_nodes);
-
+#ifdef DEBUG
 		printf("nb_nodes: %d\n", graph.nb_nodes);
-
+#endif
 		if (!(graph.nodes = malloc(sizeof(t_node) * graph.nb_nodes)))
 			return (error_exit("nodes malloc\n"));
 
 		if (!(graph.edges = malloc(sizeof(int*) * graph.nb_nodes)))
 			return (error_exit("matrix malloc\n"));
 		for (int i = 0; i < graph.nb_nodes; ++i)
-			if (!(graph.edges[i] = malloc(sizeof(int) * graph.nb_nodes)))
+			if (!(graph.edges[i] = calloc(sizeof(int), graph.nb_nodes)))
 				return (error_exit("edge malloc\n"));
 
 		for (int i = 0; i < graph.nb_nodes; ++i)
 			if (!(create_node(&graph.nodes[i], i, graph.edges)))
 				return (error_exit("create_node\n"));
 		scanf("%d", &nb_paths);
+#ifdef DEBUG
+		printf("%d\n", nb_paths);
+#endif
+//		print_matrix(graph.edges, graph.nb_nodes);
 		while (--nb_paths >= 0)
 			solve(&graph);
-
-		scanf("%s\n", tmp);
+		scanf("%d", &nb_paths);
 	}
 	return (EXIT_SUCCESS);
 }
